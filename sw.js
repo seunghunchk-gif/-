e63470e1484a369636342f6f1a8d860556b95d71
seunghunchk-gif <seunghunchk@gmail.com>
2026-09-22 08:04:00 +0900

    Add secure mobile cloud sync

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C="gyodong-v1-fix4";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="gyodong-v1-sync1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
