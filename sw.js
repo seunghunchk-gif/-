9bdabd7f0a938848e72f6ef1ce1fdd76df5b5902
seunghunchk-gif <seunghunchk@gmail.com>
2026-09-22 18:12:05 +0900

    Force mobile connection code reset via URL

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C="gyodong-v1-sync2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="gyodong-v1-sync3";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
